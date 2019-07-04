8e01ae61cdad579871d05667cf0aef118dde3b3e
Jim Hough <teachlife@outlook.com>
2019-07-04 13:48:27 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -43,6 +43,7 @@ class SongsController < ApplicationController
     genres = params[:genre][:name].map do |genre|
       Genre.find_by(:name => genre)
     end
+    binding.pry
     @song.genres = genres
     flash[:message]= "Successfully updated song."
     redirect "/songs/#{song.slug}"
